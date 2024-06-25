1300d61c01d9c33f7ba367c650c4ea05a8dd3adf
willfynch <william.finzy@protonmail.com>
2024-06-25 16:48:31 +0200

    Test husky

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,62 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import { Metadata } from "next";
+import { FaCoffee } from "react-icons/fa";
+
+export const metadata: Metadata = {
+  title: "Du Café et du Code. Studio de création de sites internet sur mesure.",
+  description:
+    "Le studio Du Café et du Code réalise des sites internet sur mesure pour les indépendants de tous les horizons grâce à des outils techniques récents et une tarification adaptée.",
+  keywords: [
+    "Développeur",
+    "Créateur de sites",
+    "Du café et du code",
+    "Combien coûte un site web",
+    "Création site web",
+    "Comment créer un site web",
+    "Développeur Normandie",
+    "Développeur Web",
+    "Développeur Rouen",
+    "Développeur",
+    "Site internet",
+    "Site vitrine",
+    "Site",
+    "Site e-commerce",
+    "E-commerce",
+    "TPE",
+    "Entrepreneurs",
+    "Sites jamstack",
+    "Sites sans serveur",
+    "Site sans wordpress",
+    "Conception de site web",
+  ],
+  metadataBase: new URL("https://ducafeetducode.com"),
+  openGraph: {
+    type: "website",
+    url: "https://ducafeetducode.com",
+    title:
+      "Du Café et du Code. Studio de création de sites internet sur mesure.",
+    description:
+      "Le studio Du Café et du Code réalise des sites internet sur mesure pour les indépendants de tous les horizons grâce à des outils techniques récents et une tarification adaptée.",
+    siteName: "Du Café et du Code",
+    images: [
+      {
+        url: "",
+      },
+    ],
+  },
+};
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+    <main>
+      <h1>
+        Plus qu’un <span className="text-yellow">site web</span>, <br />
+        Votre <span classNam e="text-yellow">signature</span>.
+      </h1>
+      <h2>Tous les sites web se ressemblent</h2>
+      <button className="btn-primary">Prendre RDV</button>
+      <span className="btn-rounded-secondary">
+        10+ sites réalisés <FaCoffee />
+      </span>
     </main>
   );
 }
