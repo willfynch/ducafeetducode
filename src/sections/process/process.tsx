6cd784fd21b47debb0e918768b68ea51808cc555
willfynch <william.finzy@protonmail.com>
2024-07-15 21:19:06 +0200

    Fix after lint

--- src/sections/process/process.tsx
+++ src/sections/process/process.tsx
@@ -2,8 +2,7 @@ import style from "./process.module.css";
 import { ReactNode } from "react";
 
 function ProcessSection(): ReactNode {
-  const DETAILS =
-    "In case you’re needing to correct for possible capitalization inconsistencies in your data attributes, the attribute selector has a case-insensitive variant for that.";
+  
   return (
     <section className={style.section}>
       <div className={style.text_container}>
