f252db81aebfcf6897322355231f1dea5063e69f
willfynch <william.finzy@protonmail.com>
2024-07-18 22:34:13 +0200

    Fix after linting

--- src/models/process-step.model.ts
+++ src/models/process-step.model.ts
@@ -1,5 +1,3 @@
-import { ReactNode } from "react";
-
 export interface ProcessCardModel {
     image: string;
     number: 'étape 1' | 'étape 2' | 'étape 3' | 'étape 4'| 'étape 5'| 'étape 6' | 'étape 7';
